8faa895a5fe3d128219a90f8ed04e7b793ddfbcc
VIPUL SHARMA <111172833+vipul2702@users.noreply.github.com>
2022-10-15 08:25:58 +0530

    Update Assignment_02.c

--- Assignment_02.c
+++ Assignment_02.c
@@ -106,6 +106,19 @@ int main()
 
 // 08) WAP to check whether the given number is even or odd using bitwise operator.
 
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter a number");
+    scanf("%d",&x);
+    if((x&1)==0)
+        printf("Even number");
+    else 
+        printf("Odd number");
+    return 0;
+}
+
 // 09) WAP to print size of an int, a float, a char and a double type variable.
 
 #include<stdio.h>
